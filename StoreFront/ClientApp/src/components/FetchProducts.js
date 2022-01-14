beb4a80c48560c96d36e14c9bb93315de23adc40
isakK96 <isak.koski96@gmail.com>
2022-01-14 13:56:36 +0100

    add search function

--- StoreFront/ClientApp/src/components/FetchProducts.js
+++ StoreFront/ClientApp/src/components/FetchProducts.js
@@ -2,6 +2,7 @@
 import ProductThumbnail from "./ProductThumbnail";
 const FetchProducts = () => {
   const [products, setProducts] = useState(null);
+  const [filteredProducts, setFilteredProducts] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -10,29 +11,51 @@ const FetchProducts = () => {
       const result = await fetch("api/products");
       const data = await result.json();
       setProducts(data);
+      setFilteredProducts(data);
       setLoading(false);
       console.log(data);
     };
     fetchProducts();
   }, []);
 
+  const handleSearch = (event) => {
+    let keyword = event.target.value.toLowerCase();
+    if (keyword !== '') {
+      const results = products.filter((product) => {
+        return product.title.toLowerCase().includes(keyword.toLowerCase());
+        // Use the toLowerCase() method to make it case-insensitive
+      });
+      setFilteredProducts(results);
+    } else {
+      setFilteredProducts(products);
+      // If the text field is empty, show all users
+    }
+
+    console.log(filteredProducts)
+  };
+
   return (
-    <div className="products-container">
-      {loading
-        ? "loading..."
-        : products.map((product) => {
-            return (
-              <ProductThumbnail
-                id={product.id}
-                key={product.id}
-                title={product.title}
-                image={product.image}
-                price={product.price}
-                category={product.category}
-              />
-            );
-          })}
-    </div>
+    <>
+      <label>Search: </label>
+      <input type="text" onChange={(event) => handleSearch(event)} />
+
+      <div className="products-container">
+        {loading
+          ? "loading..."
+          : filteredProducts.map((product) => {
+              return (
+                <ProductThumbnail
+                  id={product.id}
+                  key={product.id}
+                  title={product.title}
+                  image={product.image}
+                  price={product.price}
+                  category={product.category}
+                />
+              );
+            })}
+      </div>
+    </>
   );
 };
 
